b435b674baed23832082560450c8631f7e0bd68a
msalas555 <salasmichael2004@gmail.com>
2024-09-28 12:40:41 -0400

    update

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,12 +7,22 @@ def emotion_detector(text_to_analyse):
     response = requests.post(url,json = jsoninput,headers=header)
     formatted_response = json.loads(response.text)
     
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    dom_emotion_val = max(emotions.values())
+    if response.status_code == 200:
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        dom_emotion_val = max(emotions.values())
 
-    for e in emotions:
-        if emotions[e] == dom_emotion_val:
-            emotions['dominant_emotion'] = e
-            break
+        for e in emotions:
+            if emotions[e] == dom_emotion_val:
+                emotions['dominant_emotion'] = e
+                break
+    elif response.status_code == 400:
+        emotions = {
+            'anger': None, 
+            'disgust': None, 
+            'fear': None, 
+            'joy': None, 
+            'sadness': None, 
+            'dominant_emotion': None
+            }
 
     return emotions
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+''' Executing this function initiates the application of emotion
+    detector to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,12 +9,21 @@ app = Flask('Emotion Detector')
 
 @app.route("/emotionDetector")
 def emo_detector():
+    '''This function receives the text from index.html and runs 
+       the emotion_detector function '''
     text_to_analyze = request.args['textToAnalyze']
     response = emotion_detector(text_to_analyze)
-    return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+    return f"For the given statement, the system response is \
+        'anger': {response['anger']}, 'disgust': {response['disgust']}, \
+        'fear': {response['fear']}, 'joy': {response['joy']}\
+        and 'sadness': {response['sadness']}. The dominant\
+        emotion is {response['dominant_emotion']}."
 
 @app.route('/')
 def render_index():
+    '''Renders the index.html template'''
     return render_template("index.html")
 
 if __name__ == "__main__":
